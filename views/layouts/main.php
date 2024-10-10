24f6038080ac31ce5b5d786d3cfb78e68fbd6ff9
moniruzzaman.saikat <saikat.moniruzzaman@gmail.com>
2024-10-11 01:20:21 +0600

    fixed routes

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -44,10 +44,7 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
             ['label' => 'Test', 'url' => ['/site/test']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest
-                ? [
-                    ['label' => 'Login', 'url' => ['/site/login'], 'options' => ['class' => 'nav-item', 'style' => ['margin-left' => 'auto']]],
-                    ['label' => 'Signup', 'url' => ['/site/signup'], 'options' => ['class' => 'nav-item', 'style' => ['margin-left' => 'auto']]]
-                ]
+                ?   ['label' => 'Login', 'url' => ['/site/login'], 'options' => ['class' => 'nav-item', 'style' => ['margin-left' => 'auto']]]
                 : '<li class="nav-item">'
                     . Html::beginForm(['/site/logout'])
                     . Html::submitButton(
