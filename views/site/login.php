2b5761b5004da037b38ccdd4429527af5a0abbc7
moniruzzaman.saikat <saikat.moniruzzaman@gmail.com>
2024-10-11 01:25:09 +0600

    form layout

--- views/site/login.php
+++ views/site/login.php
@@ -17,39 +17,41 @@ $this->params['breadcrumbs'][] = $this->title;
     <p>Please fill out the following fields to login:</p>
 
     <div class="row">
-        <div class="col-lg-5">
+        <div class="col-lg-5 offset-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <?php $form = ActiveForm::begin([
+                        'id' => 'login-form',
+                        'fieldConfig' => [
+                            'template' => "{label}\n{input}\n{error}",
+                            'labelOptions' => ['class' => 'form-label'],
+                            'inputOptions' => ['class' => 'form-control'],
+                            'errorOptions' => ['class' => 'invalid-feedback'],
+                        ],
+                    ]); ?>
 
-            <?php $form = ActiveForm::begin([
-                'id' => 'login-form',
-                'fieldConfig' => [
-                    'template' => "{label}\n{input}\n{error}",
-                    'labelOptions' => ['class' => 'col-lg-1 col-form-label mr-lg-3'],
-                    'inputOptions' => ['class' => 'col-lg-3 form-control'],
-                    'errorOptions' => ['class' => 'col-lg-7 invalid-feedback'],
-                ],
-            ]); ?>
+                    <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
 
-            <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+                    <?= $form->field($model, 'password')->passwordInput() ?>
 
-            <?= $form->field($model, 'password')->passwordInput() ?>
+                    <?= $form->field($model, 'rememberMe')->checkbox([
+                        'template' => "<div class=\"custom-control custom-checkbox\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
+                    ]) ?>
 
-            <?= $form->field($model, 'rememberMe')->checkbox([
-                'template' => "<div class=\"custom-control custom-checkbox\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
-            ]) ?>
+                    <div class="form-group">
+                        <div>
+                            <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                        </div>
+                    </div>
 
-            <div class="form-group">
-                <div>
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                    <?php ActiveForm::end(); ?>
+
+                    <div style="color:#999;">
+                        You may login with <strong>admin/admin</strong> or <strong>demo/demo</strong>.<br>
+                        To modify the username/password, please check out the code <code>app\models\User::$users</code>.
+                    </div>
                 </div>
             </div>
-
-            <?php ActiveForm::end(); ?>
-
-            <div style="color:#999;">
-                You may login with <strong>admin/admin</strong> or <strong>demo/demo</strong>.<br>
-                To modify the username/password, please check out the code <code>app\models\User::$users</code>.
-            </div>
-
         </div>
     </div>
 </div>
