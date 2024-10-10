058437b9a454e0f4920ca12c1fb5158527844166
moniruzzaman.saikat <saikat.moniruzzaman@gmail.com>
2024-10-11 01:16:42 +0600

    show username

--- views/site/index.php
+++ views/site/index.php
@@ -5,7 +5,7 @@
 <div class="site-index">
 
     <div class="jumbotron text-center bg-transparent mt-5 mb-5">
-        <h1 class="display-4">Congratulations!</h1>
+        <h1 class="display-4">Welcome Back <?= Yii::$app->user->isGuest ? '' : ', ' . Yii::$app->user->identity->username ?>!</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
