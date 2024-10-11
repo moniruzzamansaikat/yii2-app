7b0e029331824332e777c8f8dee79e6b30e28044
moniruzzaman.saikat <saikat.moniruzzaman@gmail.com>
2024-10-11 12:08:48 +0600

    shared views

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -32,46 +32,7 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
 <body class="d-flex flex-column h-100">
     <?php $this->beginBody() ?>
 
-    <header id="header">
-        <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-            <div class="container">
-                <a class="navbar-brand" href="<?= Yii::$app->homeUrl ?>"><?= Html::encode(Yii::$app->name) ?></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item">
-                            <a class="nav-link  <?= isActive('/site/index') ?>" href="/site/index">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link <?= isActive('/site/about') ?>" href="/site/about">About</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/site/test">Test</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link <?= isActive('/site/contact') ?>" href="/site/contact">Contact</a>
-                        </li>
-                        <?php if (Yii::$app->user->isGuest): ?>
-                            <li class="nav-item">
-                                <a class="nav-link <?= isActive('/site/login') ?>" href="/site/login">Login</a>
-                            </li>
-                        <?php else: ?>
-                            <li class="nav-item">
-                                <form action="/site/logout" method="post" class="form-inline">
-                                    <input type="hidden" name="_csrf" value="<?= Yii::$app->request->csrfToken ?>">
-                                    <button type="submit" class="nav-link btn btn-danger logout">
-                                        Logout (<?= Yii::$app->user->identity->username ?>)
-                                    </button>
-                                </form>
-                            </li>
-                        <?php endif ?>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </header>
+    <?= $this->render('../site/shared/_header.php') ?>
 
     <main id="main" class="flex-shrink-0" role="main">
         <div class="container">
@@ -83,14 +44,7 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
         </div>
     </main>
 
-    <footer id="footer" class="mt-auto py-3 bg-light">
-        <div class="container">
-            <div class="row text-muted">
-                <div class="col-md-6 text-center text-md-start">&copy; My Company <?= date('Y') ?></div>
-                <div class="col-md-6 text-center text-md-end"><?= Yii::powered() ?></div>
-            </div>
-        </div>
-    </footer>
+    <?= $this->render('../site/shared/_footer.php') ?>
 
     <?php
         if (isset($this->blocks['scripts'])) {
---
+++ views/site/shared/_header.php
@@ -0,0 +1,44 @@
+<?php
+
+use yii\helpers\Html;
+?>
+<header id="header">
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="<?= Yii::$app->homeUrl ?>"><?= Html::encode(Yii::$app->name) ?></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link  <?= isActive('/site/index') ?>" href="/site/index">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link <?= isActive('/site/about') ?>" href="/site/about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/site/test">Test</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link <?= isActive('/site/contact') ?>" href="/site/contact">Contact</a>
+          </li>
+          <?php if (Yii::$app->user->isGuest): ?>
+            <li class="nav-item">
+              <a class="nav-link <?= isActive('/site/login') ?>" href="/site/login">Login</a>
+            </li>
+          <?php else: ?>
+            <li class="nav-item">
+              <form action="/site/logout" method="post" class="form-inline">
+                <input type="hidden" name="_csrf" value="<?= Yii::$app->request->csrfToken ?>">
+                <button type="submit" class="nav-link btn btn-danger logout">
+                  Logout (<?= Yii::$app->user->identity->username ?>)
+                </button>
+              </form>
+            </li>
+          <?php endif ?>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</header>
